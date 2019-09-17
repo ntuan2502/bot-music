79d1f76c1ac0a25f805b834df27c90474f94f99f
Tuan Nguyen <tuanng@pixiostudio.com>
2019-09-17 12:03:05 +0700

    update

--- commands/time.js
+++ commands/time.js
@@ -5,6 +5,7 @@ module.exports = {
 	description: 'Lấy thời gian hiện tại.',
 	execute(message) {
 		var today = new Date().toLocaleString("en-US", {hour: '2-digit', timeZone: "Asia/Ho_Chi_Minh"});
+		totay = new Date(today);
 		var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
 		var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 		var dateTime = time + ' ' + date;
