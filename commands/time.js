bf7c1cb6f39ddb004524bcc81d155801c6c6e5c7
Tuan Nguyen <tuanng@pixiostudio.com>
2019-09-17 12:16:26 +0700

    update

--- commands/time.js
+++ commands/time.js
@@ -4,10 +4,21 @@ module.exports = {
 	name: 'time',
 	description: 'Lấy thời gian hiện tại.',
 	execute(message) {
-		var today = new Date().toLocaleString("en-US", {hour: '2-digit', timeZone: "Asia/Ho_Chi_Minh"});
-		totay = new Date(today);
-		var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
-		var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+		function calcTime(offset) {
+			d = new Date();
+			utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+			nd = new Date(utc + (3600000*offset));
+			return nd;
+		}
+		function addZero(i) {
+			if (i < 10) {
+			  i = "0" + i;
+			}
+			return i;
+		  }
+		var today = calcTime('+7');
+		var date = addZero(today.getDate()) + '/' + addZero((today.getMonth() + 1)) + '/' + today.getFullYear();
+		var time = addZero(today.getHours()) + ":" + addZero(today.getMinutes()) + ":" + today.getSeconds();
 		var dateTime = time + ' ' + date;
 		console.log(`Bây giờ là ${dateTime}`);
 		message.channel.send(`Bây giờ là ${dateTime}`);
